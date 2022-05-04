f833bfb848113609b21f4b563570a79ae005da57
Nick <bragtn@hva.nl>
2022-05-04 22:46:46 +0200

    Add Wallet model for Mongoose

---
+++ src/models/Wallet.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+export type IWallet = mongoose.Document & {
+    _id: string;
+    staking: boolean;
+    signedUpAt: Date;
+    lastActiveAt: Date;
+};
+
+const walletSchema = new mongoose.Schema(
+    {
+        _id: String,
+        staking: Boolean,
+        signedUpAt: Date,
+        lastActiveAt: Date,
+    },
+    { timestamps: true },
+);
+
+export const Wallet = mongoose.model<IWallet>('Wallet', walletSchema, 'wallets');
