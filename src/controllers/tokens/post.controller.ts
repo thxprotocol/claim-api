2afbbefd21228558a0f8bbf642deda2b283125f9
wooyh <yung.hui.woo@hva.nl>
2022-05-05 21:24:36 +0200

    Code clean up and added keyphrase check

--- src/controllers/tokens/post.controller.ts
+++ src/controllers/tokens/post.controller.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from 'express';
 import TokenService from '@/services/TokenService';
+import { INSERT_WALLET_KEY } from '@/config/secrets';
 
 export const InsertToken = async (_req: Request, res: Response) => {
     let msg!: string;
     let status = 500;
 
-    console.log(_req.body.address);
+    if (_req.body.key != INSERT_WALLET_KEY) {
+        status = 401;
+        msg = 'Incrorrect or no keyphrase provided';
+        res.status(status).send(msg);
+    }
+
     const isTokenPresent = await TokenService.findTokenByAddress(_req.body.address);
-    console.log("> " + isTokenPresent);
+
     if (isTokenPresent) {
         msg = 'Token is already in database';
     } else {
         status = 200;
         await TokenService.addToken(_req.body.address, _req.body.type);
         msg = 'Token added to the database';
-
     }
 
     res.status(status).send(msg);
