fb0bfd423c3ad574fd437231f2ae4393b4cdaeb1
Nick <bragtn@hva.nl>
2022-05-07 17:46:56 +0200

    Resolved find wallet bug

--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -2,8 +2,12 @@ import { Wallet } from '@/models/Wallet';
 
 export default class WalletService {
 
+    static async getAllWallets(){
+        return Wallet.find();
+    }
+
     static findByWallet(address: string) {
-        return Wallet.findOne({ address });
+        return Wallet.findOne({_id: address });
     }
 
     static async addWallet(address: string){
