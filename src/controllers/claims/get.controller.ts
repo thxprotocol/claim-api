51d6a3f64da2288f3ce0e675dd20d10a0a1d53fc
wooyh <yung.hui.woo@hva.nl>
2022-05-17 22:43:24 +0200

    Added GET endpoint that checks if the wallet already exists.

--- src/controllers/claims/get.controller.ts
+++ src/controllers/claims/get.controller.ts
@@ -1,6 +1,7 @@
-import {Request, Response} from "express";
+import { Request, Response } from 'express';
+import WalletService from '@/services/WalletService';
 
 export const FindWallet = async (_req: Request, res: Response) => {
-    };
-
-
+    const response = await WalletService.isWalletExisting(_req.body.wallet);
+    res.status(200).send(response);
+};
--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -1,18 +1,21 @@
 import { Wallet } from '@/models/Wallet';
 
 export default class WalletService {
-
-    static async getAllWallets(){
+    static async getAllWallets() {
         return Wallet.find();
     }
 
     static findByWallet(address: string) {
-        return Wallet.findOne({_id: address });
+        return Wallet.findOne({ _id: address });
     }
 
-    static async addWallet(address: string){
+    static async addWallet(address: string) {
         return await Wallet.create({
-            _id: address
+            _id: address,
         });
+    }
+
+    static isWalletExisting(address: string) {
+        return Wallet.exists({ _id: address });
     }
 }
