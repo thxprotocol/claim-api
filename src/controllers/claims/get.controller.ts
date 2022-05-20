aeed8129a91b691e7b21c61d3aa5184a24a3e3b4
yunghuiwoo <yung.hui.woo@hva.nl>
2022-05-20 15:15:44 +0200

    Change end-point

--- src/controllers/claims/get.controller.ts
+++ src/controllers/claims/get.controller.ts
@@ -3,5 +3,8 @@ import WalletService from '@/services/WalletService';
 
 export const FindWallet = async (_req: Request, res: Response) => {
     const response = await WalletService.isWalletExisting(_req.body.wallet);
-    res.status(200).send(response);
+    if (response) {
+        res.status(200).send(true);
+    }
+    res.status(200).send(false);
 };
