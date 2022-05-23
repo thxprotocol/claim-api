ed322536cc8e46c808698304897285a43eda9cdb
yunghuiwoo <yung.hui.woo@hva.nl>
2022-05-23 21:29:16 +0200

    Clean up

--- src/controllers/claims/get.controller.ts
+++ src/controllers/claims/get.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import WalletService from '@/services/WalletService';
 
 export const FindWallet = async (_req: Request, res: Response) => {
-    const response = await WalletService.isWalletExisting(_req.body.wallet);
+    const response = await WalletService.isWalletExisting(_req.params.wallet);
     if (response) {
         res.status(200).send(true);
     } else {
